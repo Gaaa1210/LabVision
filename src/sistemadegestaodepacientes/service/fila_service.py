c8f0583fdb2acc2e3c8d5b5bcab4c393343a610e
ygorvpontes <161664460+ygorvpontes@users.noreply.github.com>
2025-06-13 21:08:30 -0300

    intanciando o fila_exames_repository no __init__

--- src/sistemadegestaodepacientes/service/fila_service.py
+++ src/sistemadegestaodepacientes/service/fila_service.py
@@ -1,10 +1,12 @@
 from sistemadegestaodepacientes.repository.fila_atendimento_medico_repository import FilaAtendimentoMedicoRepository
+from sistemadegestaodepacientes.repository.fila_exames_repository import FilaExamesRepository
 
 from sistemadegestaodepacientes.model.prioridade_cor import PriridadeCor
 
 class FilaService:
     def __init__(self):
         self.fila_repository = FilaAtendimentoMedicoRepository()
+        self.fila_exames_repository = FilaExamesRepository()  # 💡 Correção: adicionada aqui!
 
     def get_proximo_paciente_atendimento(self):
         fila = self.fila_repository.get_fila()
@@ -13,32 +15,22 @@ class FilaService:
             return None
         
         def prioridade(paciente):
-            cor_valor  = PriridadeCor.get_valor_prioridade(paciente.cor_prioridade)
-
-            idade_valor = 0 if paciente >= 60 or paciente.idade <= 5 else 1
-
-            return(cor_valor, idade_valor,paciente.data_chegada)
+            cor_valor = PriridadeCor.get_valor_prioridade(paciente.cor_prioridade)
+            idade_valor = 0 if paciente.idade >= 60 or paciente.idade <= 5 else 1
+            return (cor_valor, idade_valor, paciente.data_chegada)
     
         fila_ordenada = sorted(fila, key=prioridade)
-
         return fila_ordenada[0]
-    
+
     def remover_paciente_da_fila_atendimento(self, paciente):
-
         sucesso = self.fila_repository.remover_paciente(paciente)
-
         if not sucesso:
-
-            raise ValueError("paciete não está na fila de atendimento")
-
+            raise ValueError("Paciente não está na fila de atendimento.")
         return sucesso 
-    
+
     def mover_para_fila_exames(self, paciente):
         removido = self.remover_paciente_da_fila_atendimento(paciente)
-
         if removido:
             self.fila_exames_repository.adicionar_paciente(paciente)
             return True
-    
         return False
-

dbc77eaae752be93fd4488eb86726eae8d640b11
ygorvpontes <161664460+ygorvpontes@users.noreply.github.com>
2025-06-13 21:31:59 -0300

    class paciete repository concluida

--- src/sistemadegestaodepacientes/repository/paciente_repository.py
+++ src/sistemadegestaodepacientes/repository/paciente_repository.py
@@ -1,1 +1,19 @@
-#Parei aqui! 
+class PacienteRepository:
+    def __init__(self):
+        self.pacientes = []  # Lista que armazena todos os pacientes
+
+    def adicionar_paciente(self, paciente):
+        self.pacientes.append(paciente)
+
+    def buscar_por_id(self, paciente_id):
+        for paciente in self.pacientes:
+            if paciente.id == paciente_id:
+                return paciente
+        return None
+
+    def atualizar(self, paciente_atualizado):
+        for idx, paciente in enumerate(self.pacientes):
+            if paciente.id == paciente_atualizado.id:
+                self.pacientes[idx] = paciente_atualizado
+                return True
+        return False
